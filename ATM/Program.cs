60ab76e04cdffde96c0995664e24cf1d39b696cd
lamprianidis <slamprianidis@trigonongroup.com>
2024-11-15 15:55:08 +0200

    add postgresql

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -101,7 +101,6 @@ public class Program
         }
         //exit
 
-
     }     
 
 }
--- ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
+++ ATM/obj/Debug/net8.0/ATM.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5072d4f787924c3d20c7ad7b77ce95101991ccf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
